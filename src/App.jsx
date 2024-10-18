7e1ff59409f9c30f8299c8e7b93aafb8947a0574
ajitvaniya <vaniyaajit@gmail.com>
2024-10-18 11:28:20 +0530

    CSS Framework - tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
 
-function App() {
-  
-
+function App() { 
   return (
-    <> 
-      <h1>Dynamic JSON Form app</h1>
-       
+    <>  
+      <h1 className="text-3xl font-bold underline">
+        Dynamic JSON Form App
+      </h1>
     </>
   )
 }
