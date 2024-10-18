35be8557026787f19081228d5df14a45aa1e565d
ajitvaniya <vaniyaajit@gmail.com>
2024-10-18 14:30:12 +0530

    Routing title updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
+import { useLocation } from "react-router-dom";
+import { paths } from "../constants";
 
 const Header = () => {
-
+    const location = useLocation();
+    
+    let currentPath = paths.filter((path)=> path.path === location.pathname); 
 
   return (
     <div className="flex justify-between items-center mb-8">
-        <h1 className="text-3xl font-bold">{document.title}</h1>
+        <h1 className="text-3xl font-bold">{currentPath && currentPath.length>0 ?currentPath[0]?.title: 'Personal'}</h1>
         <div className="flex items-center space-x-4">
         <i className="fas fa-bell"></i>
         <div className="w-10 h-10 rounded-full bg-gray-300"></div>
