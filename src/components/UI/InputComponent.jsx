67e46aa6a2256d8f617d884f22c53dec82b6ff31
ajitvaniya <vaniyaajit@gmail.com>
2024-10-18 13:14:45 +0530

    Dymamic Form Added

---
+++ src/components/UI/InputComponent.jsx
@@ -0,0 +1,161 @@
+const InputComponent=({formData={},field={},fieldIndex=0,handleInputChange,errors={}})=>{ 
+
+    if(field.name==='subjects'){
+        console.log(`subjects----->`,Object.keys(formData[field.name]));
+        console.log(`field.options`,field.options)
+    }
+    switch (field.type) {
+        case "text":
+            return (
+                <div key={fieldIndex} className="col-span-1">
+                    <label className="block text-sm font-medium text-gray-700">{field.label}</label>
+                    <input
+                        type="text"
+                        value={formData[field.name] || ''}
+
+                        name={field.name}
+                        placeholder={field.placeholder}
+                        required={field.required}
+                        onChange={handleInputChange}
+                        className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+                    />
+
+                    {errors[field.name] && (
+                        <p className="text-red-500 text-sm mt-1">{errors[field.name]}</p>
+                    )}
+                </div>
+            );
+        case 'number': // Adding support for number type
+            return (
+              <div key={fieldIndex} className="col-span-1">
+                <label className="block text-sm font-medium text-gray-700">
+                  {field.label} {field.required && <span className="text-red-500">*</span>}
+                </label>
+                <input
+                  type={field.type}
+                  name={field.name}
+                  placeholder={field.placeholder}
+                  required={field.required}
+                  value={formData[field.name] || ''}
+                  min={field.min || ''}
+                  max={field.max || ''}
+                  onChange={handleInputChange}
+                  className={`mt-1 block w-full p-2 border ${
+                    errors[field.name] ? 'border-red-500' : 'border-gray-300'
+                  } rounded-md`}
+                />
+                {errors[field.name] && (
+                  <p className="text-red-500 text-sm mt-1">{errors[field.name]}</p>
+                )}
+              </div>
+            );
+        case "textarea":
+            return (
+                <div key={fieldIndex} className="col-span-1">
+                    <label className="block text-sm font-medium text-gray-700">{field.label}</label>
+                    <textarea
+                        name={field.name}
+                        value={formData[field.name] || ''} 
+                        placeholder={field.placeholder}
+                        required={field.required}
+                        onChange={handleInputChange}
+                        className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+                    ></textarea>
+                    {errors[field.name] && (
+                        <p className="text-red-500 text-sm mt-1">{errors[field.name]}</p>
+                    )}
+                </div>
+            );
+        case "dropdown":
+            return (
+                <div key={fieldIndex} className="col-span-1">
+                    <label className="block text-sm font-medium text-gray-700">{field.label}</label>
+                    <select
+                        name={field.name}
+                        value={formData[field.name] || ''}
+                        required={field.required}
+                        onChange={handleInputChange}
+                        className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+                    >
+                        <option value="">Select</option>
+                        {field.options.map((option, optionIndex) => (
+                            <option key={optionIndex} value={option}>
+                                {option}
+                            </option>
+                        ))}
+                    </select>
+                    {errors[field.name] && (
+                        <p className="text-red-500 text-sm mt-1">{errors[field.name]}</p>
+                    )}
+                </div>
+            );
+        case "radio":
+            return (
+                <div key={fieldIndex} className="col-span-2">
+                    <label className="block text-sm font-medium text-gray-700">{field.label}</label>
+                    <div className="mt-2 space-x-4">
+                        {field.options.map((option, optionIndex) => (
+                            <label key={optionIndex} className="inline-flex items-center">
+                                <input
+                                    type="radio"
+                                    name={field.name}
+                                    value={option.value?true:false}
+                                    onChange={handleInputChange}
+                                    className="form-radio"
+                                />
+                                <span className="ml-2">{option.label}</span>
+                            </label>
+                        ))}
+                    </div>
+                    {errors[field.name] && (
+                        <p className="text-red-500 text-sm mt-1">{errors[field.name]}</p>
+                    )}
+                </div>
+            );
+        case "checkbox":
+            return (
+                <div key={fieldIndex} className="col-span-2">
+                    <label className="block text-sm font-medium text-gray-700">{field.label}</label>
+                    <div className="mt-2 space-x-4">
+                        {field.options.map((option, optionIndex) => (
+                            <label key={optionIndex} className="inline-flex items-center">
+                                <input
+                                    type="checkbox"
+                                    name={field.name}
+                                    checked={Object.keys(formData[field.name]).includes(option.value)?true:false}
+                                    onChange={(e)=>handleInputChange(e,option)}
+                                    className="form-checkbox"
+                                />
+                                <span className="ml-2">{option.label}</span>
+                            </label>
+                        ))}
+                    </div>
+                    {errors[field.name] && (
+                        <p className="text-red-500 text-sm mt-1">{errors[field.name]}</p>
+                    )}
+                </div>
+            );
+        case "slider":
+            return (
+                <div key={fieldIndex} className="col-span-2">
+                    <label className="block text-sm font-medium text-gray-700">{field.label}</label>
+                    <input
+                        type="range"
+                        name={field.name}
+                        min={field.min}
+                        max={field.max}
+                        value={formData[field.name] || ''}   
+                        step={field.step}
+                        onChange={handleInputChange}
+                        className="mt-1 block w-full"
+                    />
+                    {errors[field.name] && (
+                        <p className="text-red-500 text-sm mt-1">{errors[field.name]}</p>
+                    )}
+                </div>
+            );
+        default:
+            return null;
+    }
+}
+export default InputComponent;
--- src/pages/Personal.jsx
+++ src/pages/Personal.jsx
@@ -1,13 +1,103 @@
+import  { useState,useEffect  } from 'react';
+import formJson from '../data/formdata.json'; 
+import InputComponent from '../components/UI/InputComponent';
 
 
 function Personal() {
-  return (
-   <>
-    <div className="bg-white shadow-md rounded-lg p-6">
-        <h2>Personal Information Form</h2>     
-    </div>
-   </>
-  );
+    const [formData, setFormData] = useState(localStorage.getItem('applicantDetails')?JSON.parse(localStorage.getItem('applicantDetails')):{});
+    const [errors, setErrors] = useState({});
+    const [isFormValid, setIsFormValid] = useState(false);
+
+    const handleInputChange = (e,checkboxOption={}) => {
+        const { name, value, type, checked } = e.target;
+
+        if (type === "checkbox") {
+            console.log(`checkbox`,{ name, value, type, checked },checkboxOption.value);
+            setFormData({
+                ...formData,
+                [name]: {
+                    ...formData[name],
+                    [checkboxOption.value]: checked,
+                },
+            });
+        } else {
+            setFormData({
+                ...formData,
+                [name]: value,
+            });
+        } 
+        if (value) {
+            setErrors((prevErrors) => ({ ...prevErrors, [name]: null }));
+        }
+    };
+
+    const validateForm = () => {
+        let isValid = true;
+        const newErrors = {};
+    
+        formJson.form.groups.forEach((group) => {
+          group.fields.forEach((field) => { 
+            const fieldValue = formData[field.name];
+            if (field.required && !formData[field.name]) {
+              isValid = false;
+              newErrors[field.name] = `${field.label} is required.`;
+            }
+            if (field.type === 'number' && fieldValue) {
+                const value = Number(fieldValue);
+                if (field.min && value < field.min) {
+                  isValid = false;
+                  newErrors[field.name] = `${field.label} must be at least ${field.min}.`;
+                }
+                if (field.max && value > field.max) {
+                  isValid = false;
+                  newErrors[field.name] = `${field.label} cannot exceed ${field.max}.`;
+                }
+              }
+          });
+        });
+    
+        setErrors(newErrors);
+        setIsFormValid(isValid);
+    };
+
+    const handleSubmit =()=>{ 
+        localStorage.setItem('applicantDetails',JSON.stringify(formData));
+        alert('data saved successfully...!')
+    }
+
+    useEffect(() => {
+        validateForm();
+      }, [formData]);
+
+      console.log(`formData--->`,formData)
+
+    return (
+        <>
+            <div className="bg-white shadow-md rounded-lg p-6">
+                <h1 className="text-3xl font-bold mb-6">{formJson.form.title}</h1>
+                <p className="mb-6 text-gray-600">{formJson.form.description}</p>
+
+                {formJson.form.groups.map((group, groupIndex) => (
+                    <div key={groupIndex} className="mb-8">
+                        <h2 className="text-2xl font-semibold mb-4">{group.title}</h2>
+                        <div className="grid grid-cols-2 gap-4">
+                            {group.fields.map((field, fieldIndex) => {
+                                return <InputComponent key={`group_${group.title}_${fieldIndex}`} formData={formData} fieldIndex={fieldIndex} field={field} handleInputChange={handleInputChange} errors={errors} />
+                            })}
+                        </div>
+                    </div>
+                ))}
+
+                <button
+                    className={`bg-black text-white py-2 px-4 rounded-md ${!isFormValid ? 'opacity-50 cursor-not-allowed' : ''}`}
+                    disabled={!isFormValid}
+                    onClick={handleSubmit}
+                >
+                    Submit
+                </button>
+            </div>
+        </>
+    );
 }
 
 export default Personal;
